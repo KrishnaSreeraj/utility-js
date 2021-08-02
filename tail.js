c7ec65b4f530aa6f3a085d3d2417b26385ca63cc
Krishna Sreeraj <krishnasreerajkp99@gmail.com>
2021-08-02 18:54:53 +0530

    Add logic to return the tail elements of an array

---
+++ tail.js
@@ -0,0 +1,5 @@
+const tail = (array) => {
+    if (array.length === 0) return [];
+    return array.slice(1);
+}
+module.exports = tail;
---
+++ tail.spec.js
@@ -0,0 +1,13 @@
+const tail = require('./tail');
+
+describe('Tail', () => {
+
+    it('Tail of [1,2,3] is [2,3]', () => {
+        expect(tail([1, 2, 3])).toEqual([2,3]);
+    });
+
+    it('Tail of [2,5,7,9] is [5,7,9]', () => {
+        expect(tail([2, 5, 7, 9])).toEqual([7,9]);
+    });
+
+})
